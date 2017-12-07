38bbc4148dd23807f32ffbcc80cb023909af0499
BertZZ <bertie.wooles@hotmail.co.uk>
2017-12-07 17:36:27 +0000

    Kata Solution

---
+++ C#/hoop.cs
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/keep-up-the-hoop/train/csharp
+
+public class Kata
+{
+  public static string HoopCount(int n)
+  {
+    if (n >= 10)
+    {
+      return "Great, now move on to tricks";
+    }
+    else
+    {
+      return "Keep at it until you get it";
+    }
+  }
+}
