ddb1a6434844e1364b0a80981499312255940f94
BertZZ <bertie.wooles@hotmail.co.uk>
2017-12-12 20:43:09 +0000

    Very long solution to a simple problem

--- Ruby/array_diff.rb
+++ Ruby/array_diff.rb
@@ -1,5 +1,15 @@
 #https://www.codewars.com/kata/array-dot-diff/train/ruby
 
 def array_diff(a, b)
-
+  i = 0
+  (a.length).times do
+    if a[i] == b[0]
+      a.delete_at(i)
+      i -= 1
+   end
+   i += 1
+  end
+  a
 end
+
+array_diff([1,2,2], [2])
