1901227721cd927f4dfc6e24b02612bccbdd2438
BertZZ <bertie.wooles@hotmail.co.uk>
2018-04-03 14:46:45 +0100

    Solution to PHP kata

--- PHP/average.php
+++ PHP/average.php
@@ -1,4 +1,9 @@
 <?php
 // https://www.codewars.com/kata/calculate-average/train/php
-
+function find_average($array) {
+  $length = sizeof($array);
+  $sum = array_sum($array);
+  $average = $sum / $length;
+  return $average;
+}
 ?>
