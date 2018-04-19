ac8da169e2ff4afdb28dac3889ac7a53f7e1c178
BertZZ <bertie.wooles@hotmail.co.uk>
2018-04-19 20:44:45 +0100

    Adds FizzBuzz PHP

---
+++ PHP/fizzbuzz.php
@@ -0,0 +1,20 @@
+<?php
+function fizzbuzz(){
+  for($i = 1; $i < 100; $i++){
+    if ($i % 5 == 0 && $i % 3 == 0){
+      echo "FizzBuzz ";
+    }
+    else if ($i % 3 == 0){
+      echo "Fizz ";
+    }
+    else if ($i % 5 == 0){
+      echo "Buzz ";
+    }
+    else{
+      echo $i. " ";
+    }
+  }
+}
+
+fizzbuzz();
+?>
